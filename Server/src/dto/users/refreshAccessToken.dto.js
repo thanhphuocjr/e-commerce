0f0f495e5e39dc7c8ec2502e244fc87e5b5dc676
thanhphuocjr <thanhphuocjr03012005@gmail.com>
2025-12-10 21:09:56 +0700

    Delete Monolithic

--- Server/src/dto/users/refreshAccessToken.dto.js
+++
@@ -1,7 +0,0 @@
-export class RefreshTokenDTO {
-  constructor(accessToken, refreshToken, expiresIn) {
-    this.accessToken = accessToken;
-    this.refreshToken = refreshToken;
-    this.expiresIn = expiresIn;
-  }
-}
--- Server/src/utils/TokenHelper.js
+++
@@ -1,4 +0,0 @@
-import crypto from 'crypto';
-export const generateRefreshToken = () => {
-  return crypto.randomBytes(64).toString('hex');
-};
