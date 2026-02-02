d467df22e629456f1edc299a095df9269bed0727
thanhphuocjr <thanhphuocjr03012005@gmail.com>
2026-02-02 16:13:51 +0700

    Feat product-service: Add check middleware, body parsing,...

--- Services/product-service/src/server.ts
+++ Services/product-service/src/server.ts
@@ -1,14 +1,38 @@
 import express from 'express';
 import type { Request, Response } from 'express';
+import cors from 'cors';
+import cookieParser from 'cookie-parser';
+
+import helmet from 'helmet';
 
 const app = express();
 const PORT = 8001;
 
-app.use(express.json());
+app.use(helmet);
 
-app.get('/', (req: Request, res: Response) => {
+app.use(
+  cors({
+    origin: '*',
+    credentials: true,
+    methods: ['GET', 'POST', 'PUT', 'DELETE', 'PATCH'],
+    allowedHeaders: ['Content-Type', 'Authorization'],
+  }),
+);
+
+// Middleware
+
+// Body parsing
+app.use(express.json({ limit: '50mb' }));
+app.use(express.urlencoded({ limit: '50mb', extended: true }));
+app.use(cookieParser());
+
+//Health check
+
+app.get('/health', (req: Request, res: Response) => {
   res.send('Product Service is running with TypeScript');
 });
+
+// app.use('/v1/auth', createProductRoute);
 
 app.listen(PORT, () => {
   console.log('Server Product is running');
